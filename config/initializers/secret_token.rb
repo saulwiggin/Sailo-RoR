cf862760d626de7ab066e0f2b1e8af3e3e4c7359
saul <saulwiggin@googlemail.com>
2014-09-26 15:07:58 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sailo::Application.config.secret_token = '864062c90c58022c4b9485571672260e319ecb70972473f44fc9eff7d1c9e8e001239ce11bee56a6b1d855a326c186bbff11b329f41d9fed529ddd8166085257'
